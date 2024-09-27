2f641201e1434b1496572b619946ee5fce5d4bb5
Nephim <anders@nephim.dk>
2024-09-27 09:12:46 +0200

    Initial commit of templates

---
+++ src/UICrafter.Mobile/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UICrafter.Mobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
