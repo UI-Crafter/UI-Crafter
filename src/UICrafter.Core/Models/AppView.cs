1daa8c3be402d2ceff9302dd0c4108b3fe027471
Nephim <anders@nephim.dk>
2024-10-14 12:56:51 +0200

    Added UI.proto

--- src/UICrafter.Core/Models/AppView.cs
+++ src/UICrafter.Core/Models/AppView.cs
@@ -6,14 +6,14 @@ using Google.Protobuf.WellKnownTypes;
 public partial class AppView
 {
 	// Expose CreatedAt as a DateTimeOffset property
-	public DateTime CreatedAtOffset
+	public DateTime CreatedAt
 	{
 		get => CreatedAtUTC?.ToDateTime() ?? DateTime.MinValue;
 		set => CreatedAtUTC = value.ToTimestamp();
 	}
 
 	// Expose UpdatedAt as a DateTimeOffset property
-	public DateTime UpdatedAtOffset
+	public DateTime UpdatedAt
 	{
 		get => UpdatedAtUTC?.ToDateTime() ?? DateTime.MinValue;
 		set => UpdatedAtUTC = value.ToTimestamp();
