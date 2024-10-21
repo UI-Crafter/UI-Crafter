27e5dc65b45856694aba46ff1f98092bf4ff7eb1
Gormiee <91832027+Gormiee@users.noreply.github.com>
2024-10-21 14:23:09 +0200

    setup and test case for API

--- src/UICrafter.Mobile/MauiProgram.cs
+++ src/UICrafter.Mobile/MauiProgram.cs
@@ -56,6 +56,7 @@ public static class MauiProgram
 		// gRPC
 		builder.Services.AddGrpcClient<AppViewService.AppViewServiceClient>();
 
+
 		// Repository
 		builder.Services.AddScoped<IAppViewRepository, AppViewRepository>();
 
