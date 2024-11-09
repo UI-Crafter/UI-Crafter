747b76998367136d976dfddfcfe6fbef36fd6a8d
Nephim <anders@nephim.dk>
2024-11-09 02:30:38 +0100

    Adjusted UI layout and navbar to look as expected

--- src/UICrafter.Mobile/MainPage.xaml.cs
+++ src/UICrafter.Mobile/MainPage.xaml.cs
@@ -17,4 +17,14 @@ public partial class MainPage : ContentPage
 		_refreshViewState.SetIsRefreshing(true);
 		Task.Delay(1000).ContinueWith(_ => _refreshViewState.SetIsRefreshing(false));
 	}
+
+	protected override void OnHandlerChanged()
+	{
+		base.OnHandlerChanged();
+#if ANDROID
+		var blazorView = this.blazorWebView;
+		var platformView = blazorView.Handler!.PlatformView as Android.Webkit.WebView;
+		platformView!.OverScrollMode = Android.Views.OverScrollMode.Never;
+#endif
+	}
 }
