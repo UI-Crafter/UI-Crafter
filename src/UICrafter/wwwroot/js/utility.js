798030b27659b3c3d3a76b1d7ba4d31b8ee828cb
Nephim <anders@nephim.dk>
2024-11-10 14:48:43 +0100

    Fixed bug + Refactored UserProfileAvatar

--- src/UICrafter/wwwroot/js/utility.js
+++ src/UICrafter/wwwroot/js/utility.js
@@ -6,7 +6,7 @@ window.blazorUtility.setThemeCookie = function (base64Value) {
 	const date = new Date();
 	date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
 	const expires = "expires=" + date.toUTCString();
-	document.cookie = `${cookieName}=${base64Value};${expires};path=/;Secure;SameSite=Strict`;
+	document.cookie = `${cookieName}=${base64Value};${expires};path=/;Secure;SameSite=Lax`;
 }
 
 window.blazorUtility.isSystemDarkMode = function () {
