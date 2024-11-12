c4b1b3654d8f8a5a089e203db85a7d4227a86a82
Nephim <anders@nephim.dk>
2024-11-12 21:58:10 +0100

    Further optimized ReplaceLogicalNames with stringBuilder

--- src/UICrafter.Core/Utility/APICallUtility.cs
+++ src/UICrafter.Core/Utility/APICallUtility.cs
@@ -1,16 +1,18 @@
 namespace UICrafter.Core.Utility;
 
+using System.Text;
 using UICrafter.Core.UIComponents;
 
 public static class APICallUtility
 {
 	public static string ReplaceLogicalNames(IEnumerable<UIComponent> UIComponents, string s)
 	{
+		StringBuilder sb = new(s);
 		foreach (var component in UIComponents.Where(x => x.ComponentCase == UIComponent.ComponentOneofCase.InputField))
 		{
-			s = s.Replace($"{{{component.InputField.LogicalName}}}", component.InputField.Value);
+			sb.Replace($"{{{component.InputField.LogicalName}}}", component.InputField.Value);
 		}
 
-		return s;
+		return sb.ToString();
 	}
 }
